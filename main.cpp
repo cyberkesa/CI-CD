7cbfba1c1fc62cc7fab26a32829845dff64b145e
cyberkesa <nastya63rus@mail.ru>
2024-01-18 02:39:04 +0300

    Add .gitignore

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 
 int	main(int, char**) {
 	std::cout << "Version: " << version() <<std::endl;
-	std::cout << "Hello, world!" << std::endl;
+	std::cout << "Hello, world!\n" << std::endl;
 	return (0);
 }
